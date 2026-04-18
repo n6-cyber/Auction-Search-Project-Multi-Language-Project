1f0f8f6668330a79bf9cf8f151f99fb049e847ca
n6-cyber <ribaruktimothy@gmail.com>
2026-04-18 23:15:10 +0100

    Finished create app function, used for making and configing flask app instances, allowing for multi-instance testing and enviorments.

--- Auction Search Engine v.01/app.py
+++ Auction Search Engine v.01/app.py
@@ -15,4 +15,24 @@ def create_app(config_name='development'):
 
     db.init_app(app)
 
+    # don't touch this right now.
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app('development')
+    app.run(debug=True)
     
